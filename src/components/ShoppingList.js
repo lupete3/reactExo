ed15604d17d5f72847ca326f367bf18eae0fb04f
lupete3 <lupeteplacide@gmail.com>
2022-11-28 19:57:55 +0100

    Add and use best sale product in ShoppingList

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,4 @@
-
+import '../styles/ShoppingList.css'
 import { plantList } from '../datas/plantList'
 
 function ShoppingList(){
@@ -14,9 +14,12 @@ function ShoppingList(){
 					<li key={cat}>{cat}</li>
 				))}
 			</ul>
-			<ul>
+			<ul className='lmj-plant-list'>
 				{plantList.map((plant) => (
-					<li key={plant.id}>{plant.name} {(plant.isBestSale || plant.category === "classique") && <b>best</b> }</li>
+					<li className='lmj-plant-item ' key={plant.id}>
+                        {plant.name} 
+                        {plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
+                    </li>
 				))}
 			</ul>
 		</div>
